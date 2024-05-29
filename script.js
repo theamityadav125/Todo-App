eb42bbbdc814b399d5e3346197ad517262d89694
Amit Kumar Yadav <theamityadav125@gmail.com>
2024-05-29 23:26:17 +0530

    validation

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ let inputs = document.getElementById("inp");
 let text = document.querySelector(".text");
 
 function Add() {
-  if (inputs.length < 3) {
-    alert("Task should atleast more than 3 character");
-  }
   if (inputs.value == "") {
     alert("Please Enter Task");
-  } else {
+  } else if (inputs.value.length < 3) {
+    alert("Task should atleast more than 3 character");
+  }
+
+  //  else if (inputs.value.typeof === Number) {
+  //   alert("please enter valid task");
+  else {
     let newEle = document.createElement("ul");
     newEle.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`;
     text.appendChild(newEle);
